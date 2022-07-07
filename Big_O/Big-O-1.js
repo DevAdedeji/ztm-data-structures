8ee9f2a4b48c63426af4526a84104e407bc44395
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-07 16:09:15 +0100

    O(n) - linear time

---
+++ Big_O/Big-O-1.js
@@ -0,0 +1,16 @@
+const nemo = ['nemo']
+const cars = ['mercedes benz', 'audi', 'bmw', 'toyota', 'land rover', 'nissan', 'dodge', 'ford']
+const large = new Array(1000).fill('nissan');
+
+function findNemo(array) {
+	let t0 = performance.now();
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] === 'nissan') {
+			console.log('found nissan!')
+		}
+	};
+	let t1 = performance.now();
+	console.log('function took ' + (t1 - t0) + ' milliseconds')
+};
+
+findNemo(large);

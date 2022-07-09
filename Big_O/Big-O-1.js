443ff7735c446a3e71ea1d574183e257a74d0f4b
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-09 18:11:32 +0100

    forEach() and forof

--- Big_O/Big-O-1.js
+++ Big_O/Big-O-1.js
@@ -3,16 +3,35 @@ const cars = ['mercedes benz', 'audi', 'bmw', 'toyota', 'land rover', 'nissan', 
 const large = new Array(1000).fill('nissan');
 
 function findNemo(array) {
-	let t0 = performance.now();
+	// let t0 = performance.now();
 	for (let i = 0; i < array.length; i++) {
 		if (array[i] === 'nissan') {
 			console.log('found nissan!')
 		}
 	};
-	let t1 = performance.now();
-	console.log('function took ' + (t1 - t0) + ' milliseconds')
+	// let t1 = performance.now();
+	// console.log('function took ' + (t1 - t0) + ' milliseconds')
 };
 
-findNemo(large);
+const findNemo2 = array => {
+	array.forEach(car => {
+		if (car === 'nissan'){
+			console.log('found nissan')
+		}
+	})
+}
+//forEach function
+
+const findNemo3 = array => {
+	for(let car of array) {
+		if (car === 'nissan'){
+			console.log('found nissan')
+		}
+	}
+}
 
 // O(n)
+
+findNemo(cars);
+findNemo2(cars);
+findNemo3(cars);

1c21346dd8762d0b4ae9c397463bb68c1254dbd7
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-09 03:15:18 +0100

    O(1) constant time and Big O calculation

---
+++ Big_O/Big0_2.js
@@ -0,0 +1,14 @@
+const printItems = (items) => {
+	console.log(items[0]);
+	const middleIndex = Math.floor(items.length/2);
+	const index = 0;
+	
+	while(index < middleIndex){
+		console.log(items[index]);
+		index++
+	}
+
+	for (let i = 0; i < 100; i++){
+		console.log('hi')
+	}
+}

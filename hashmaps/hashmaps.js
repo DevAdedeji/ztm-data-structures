4c094e355bd0a31389b91f0960ed134198d705bc
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-17 18:18:00 +0100

    Sets and Maps

--- hashmaps/hashmaps.js
+++ hashmaps/hashmaps.js
@@ -16,3 +16,13 @@ let user = {
 console.log(user.age) // O(1) Reading
 user.scream('haaaa') // Insertion O(1)
 console.log(user)
+
+// Objects in javascript only allow strings. If passed a value of any other type, it gets stringified
+
+// New in ES6
+// A map is different from an object because it allows you to save any data type as the key. An object only allows saving tyhe key as a string.
+// A maintains insertion order.
+const a = new Map()
+const b = new Set()
+
+// Sets are very similar to objects, the only difference is that a set stores the keys and no values.

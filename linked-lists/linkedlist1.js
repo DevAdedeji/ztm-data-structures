ecf37277245897fb644132636a33c3ee13852583
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-18 18:58:09 +0100

    linked lists

---
+++ linked-lists/linkedlist1.js
@@ -0,0 +1,9 @@
+// There are two types of linked lists.
+// Singly linked lists and doubly linked lists
+// A singly linked list contain nodes with two elements.
+// The value of the data stored in the node and a pointer to the next node.
+// The first node is called the head, the last node is called the tail.
+// Linked lists are null terminated. A linked list ends when the tail node points to null.
+
+// Linked list can be sorted or unsorted.
+// Javascript dosen't come with linked lists, unlike Java etc

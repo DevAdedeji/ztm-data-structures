863c0d9d9425fcf5821baf8618338405c735fb75
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-18 18:19:18 +0100

    Add recurringItem function

--- hashmaps/recurringCharacter.js
+++ hashmaps/recurringCharacter.js
@@ -10,8 +10,22 @@
 
 
 function firstRecurringCharacter(input) {
-	
+	// Define an object for values found.
+	const valueObject = {};
+	// Loop through the array
+	for (let i = 0; i < input.length; i++) {
+		for (let j = i + 1; j < input.length; j++) {
+			if (input[i] === input[j]){
+				return input[i]
+			}
+		}
+	}
+	return undefined;
+	// Check if duplicates are found in the valuesArray
+	// If no duplicates are found, just return the array.
 }
+
+console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]))
 
 //Bonus... What if we had this:
 // [2,5,5,2,3,5,1,2,4]

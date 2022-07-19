3eb68ff61604d30eed5320eb53d68b5a103aae94
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-19 01:55:13 +0100

    pointers

--- linked-lists/linkedlist1.js
+++ linked-lists/linkedlist1.js
@@ -7,3 +7,18 @@
 
 // Linked list can be sorted or unsorted.
 // Javascript dosen't come with linked lists, unlike Java etc
+
+// Linked lists have a loose structure
+// Array and linked lists differ because elements have an index.
+// Iterating through a linked list is usually slower than an array
+// Linked lists have an order. Each node points to the next node.
+
+// Ponters:
+// A ponter is a reference to another place in memory.
+const obj1 = {a: test};
+const obj2 = obj1
+
+// Obj2 referring to obj1 is a pointer. It's just referring to an address in memory.
+// Both obj1 and obj2 point to the same point in memeory.
+
+// A ponter says this is where x is in memory

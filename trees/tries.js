cecfff56d54ac4376dd997349d0ab827946fa6f8
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-22 00:06:26 +0100

    tries

---
+++ trees/tries.js
@@ -0,0 +1,18 @@
+// A trie is a specialized tree used in searching. Most often with text.
+
+// It can outperform most data structures in some cases.
+
+// A trie allows you to know if a word or a part of a word exists in a body of text.
+// Tries have an empty root node.
+// Letters are added from there.
+
+// Tries are also called pre fix trees.
+// It can solve string specific trees.
+
+// Autocomplete, diuctionaries, word lookups etc
+
+// Big O(length of word)
+// Benefits inclued speed and space.
+
+// Space complexity is also efficient
+// They use prefixes in different words, it dosen't have to be stored in multiple places.

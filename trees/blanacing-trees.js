2b86f5a671f07f1d953ed65e5f5509932b9303f5
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-21 21:56:36 +0100

    balancing trees

---
+++ trees/blanacing-trees.js
@@ -0,0 +1,16 @@
+// Usually in production, you want to have a balanced tree.
+
+// There are two types of trees for balancing
+// AVL trees and Red black trees are usuually used to automatically create trees that balance in production.
+
+// This is to prevent the edge cases that come with unbalanced trees.
+
+// There are usually libraries etc used for implementing AVL trees.
+// AVL trees automatically balance out values that makes a tree unbalanced.
+// Further reading
+// https://www.cs.usfca.edu/~galles/visualization/AVLtree.html
+// https://medium.com/basecs/the-little-avl-tree-that-could-86a3cae410c7
+
+// Red black trees
+// https://www.cs.usfca.edu/~galles/visualization/RedBlack.html
+// https://medium.com/basecs/painting-nodes-black-with-red-black-trees-60eacb2be9a5

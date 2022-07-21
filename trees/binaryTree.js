8ef03658105299ed5fc8ee2a75ac4b31860dee52
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-21 15:37:21 +0100

    binary trees introduction

---
+++ trees/binaryTree.js
@@ -0,0 +1,19 @@
+// Each node in a binary tree can only have either zero, one or two nodes
+// Each child can only have one parent
+// Each node has a certauin state
+
+const BinaryTreeNode = () => {
+	this.value = value
+	this.left = null;
+	this.right = null;
+}
+
+// A perfect binary tree has all the nodes full
+// Nothing is missing
+
+// A full tree has one or two nodes but not one
+// The number of nodes on a perfect tree doubles with each node level
+// The bottom level of a perfect tree has its number of nodes equal to the number of the nodes in the preceeding levels plus one.
+
+// O(log N) because of the structure of trees.
+

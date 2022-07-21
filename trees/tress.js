08d6b9e150dc953d07c5b6a3a1b4ab1b5a431155
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-21 15:25:05 +0100

    trees

---
+++ trees/tress.js
@@ -0,0 +1,14 @@
+// Tress have an heirachichal data structure, as opposed to linear structures
+// Tree structure
+//        Root
+//         /|\
+//leaf    parent   leaf
+
+// A unidirectional parent child reationship
+// Trees contain sub trees.
+
+// DOM is an example of tree data structure implemented.
+// A computer in a chess game used trees to make decisions.
+
+// Trees use the same node principle as in linked lists
+// Linked liaats are technically tress with a single direction flow.

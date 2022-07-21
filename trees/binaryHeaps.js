a97d1c522aaa4a0b587332027a1cb5ea2e295ad5
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-21 23:49:35 +0100

    binary heaps

---
+++ trees/binaryHeaps.js
@@ -0,0 +1,22 @@
+// Heaps are also referred to as tries
+// Binary heaps have every child belonging to a parent node that has a higher prority
+
+// A max heap has the root node value as the highest value
+// A min heap has the root node value as the lowest value
+// The node value can be any data type
+
+// heap lookup O(n)
+// heap insert O(log n)
+// heap delete O(log n)
+
+// Are more favorable in algorithms that prioritize sorting.
+// Why use a binary heap?
+// Binary heaps are great for comparative operations
+// Heaps are used a lot in data storage, priority queues, sorting algorithms. 
+// Heaps have values from left to right.
+// The highest value bubbles up the node tree.
+
+// P.S: Memory heaps != heap data structure.
+// Heap in terms of memory is a region where arbitrary data can be stored in memory.
+
+// To understand how binary heaps work, we have to talk about priority queues.

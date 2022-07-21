427a8d176ac6e97e557d8b393d7f96d80c428066
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-21 15:56:48 +0100

    binary search trees

---
+++ trees/binarySearchTrees.js
@@ -0,0 +1,14 @@
+// Binary search trees are good at comparing data.
+
+// Binary search trees preserves relationships
+
+// All child nodes on the right should be greater than he left. The left always decreases
+
+/*
+	Lookup for a binarySearchTree is O(log N)
+
+	insert is O(log N)
+
+	delete is (O log N)
+*/
+

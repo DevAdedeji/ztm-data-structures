cb939bba2cf5a5cbf2ee7e88ddec279b56954e62
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-22 05:08:45 +0100

    base case

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -1,10 +1,18 @@
 // Recursion really isn't an algorithm. It's more of a concept.
 // Recursion is when something is defined within itself.
 // A recursive function, for example is a function that calls itself.
+let counter = 0;
 
-const recursive = () => {
-	recursive()
+const recursive1 = () => {
+	if (counter >= 5){
+		return 'done!'
+	}
+	counter++
+	console.log('hey!' + counter)
+	return recursive1()
 }
+
+console.log(recursive1())
 
 // recursive as the name suggests is a recursive function because it's going to call itself.
 // Recursion is useful for tasks with repeated tasks to do.
@@ -20,3 +28,12 @@ const recursive = () => {
 // Stack overflows are one of the biggest problems with recursion
 
 // The computer needs to allocate memeory to remember the function being called.
+
+// Every recursive function needs to have a base case
+// The function stops calling itself if the conditions in the base case is met.In the case of the recursive function above, it's when the counter is > or = 5. Once that condition is met, the function stops running.
+
+// There's usually a base case that should have a return statement so that the value wanted will be bubbled up to the top of the stack.
+
+// 1: Identify the base case
+// 2: identify the recursive case
+// 3: Get closer and closer and return when needed. Uusally 2 returns for the base case and the recursive case.

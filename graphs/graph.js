159df5b5aa354e88e5ee463543175e527d59bc05
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-22 01:10:17 +0100

    graphs

---
+++ graphs/graph.js
@@ -0,0 +1,32 @@
+// A graph is a set of values that are related in a pair wise fashion
+
+// Graphs are widely used when it comes to modelling real life.
+// Looks like a network with connections to different nodes/vertex
+
+// Nodes are connected with edges
+// Graphs are great model structures for modelling the real world
+
+// Graphs are useful for things that connect to other things. Like the internet. Graphs can be used to represent human connections(friendships etc), roads etc
+
+// Graphs encompass other data structures.
+// Linked lists for example are a type of tree.
+// Trees in return are a type of graph.
+
+// Types of graphs
+// Directed and undirected graphs
+// Traffic flow or system where movement is not bi directional
+
+// Weighted and unweigheted graph
+// Values can be applied to various aspects of a graph, not just the nodes.
+// The edges connecting the nodes in graphs also carry information
+// Google maps for example can use a weighted graph to calculate the shortest distance between two destinations.
+
+
+// A weighted graph in short is the type of graph where the edges carry sone information
+
+// Cyclic and acyclic graphs
+// When vertcices/edges are connected in cycle, there's a cyclic graph.
+
+// Acyclic dosen't allow that.
+// Cyclic graphs are especially common in weighted graphs, such as google maps.
+// Since there is a way to get back most of the time

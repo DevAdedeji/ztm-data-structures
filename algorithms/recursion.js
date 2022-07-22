860cf6fd01d86214cc99cb90755af2b720bbbaf4
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-22 04:40:30 +0100

    recursion introduction

---
+++ algorithms/recursion.js
@@ -0,0 +1,11 @@
+// Recursion really isn't an algorithm. It's more of a concept.
+// Recursion is when something is defined within itself.
+// A recursive function, for example is a function that calls itself.
+
+const recursive = () => {
+	recursive()
+}
+
+// recursive as the name suggests is a recursive function because it's going to call itself.
+// Recursion is useful for tasks with repeated tasks to do.
+// Recursion is important because it's used in searching and sorting algorithms.

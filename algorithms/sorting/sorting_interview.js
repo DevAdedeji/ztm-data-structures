98f12d6d8d684aa3dce0036fab4f63e3918678ee
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-24 20:50:34 +0100

    sorting excercise

---
+++ algorithms/sorting/sorting_interview.js
@@ -0,0 +1,28 @@
+//#1 - Sort 10 schools around your house by distance:
+// insertion
+// Inserion sort is fast andsince the input is small, it's suitable. Space complexity of O(1)
+
+//#2 - eBay sorts listings by the current Bid amount:
+// radix or counting
+// A bid is usually a number between a range.
+
+//#3 - Sport scores on ESPN
+// Quick Sort
+// Quick sort because in memory sorting because of better space complexity as opposed to merge sort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+// merge
+// Because data is big, we're woried about worst case time complexities
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+// insertion. Insertion sort for pre ordered works better
+
+//#6 - Temperature Records for the past 50 years in Canada
+// Rdaix ot counting, based on the range. If there are no decimal places
+// Quick sort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+// quick sort
+
+//#8 - You want to teach sorting for the first time
+// bubble or selection sort

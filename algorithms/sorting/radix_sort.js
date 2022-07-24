d0dabb946ae4a201e3daf0bd2bd02c5151713396
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-24 20:35:55 +0100

    non comparison sorting algorithms

---
+++ algorithms/sorting/radix_sort.js
@@ -0,0 +1,10 @@
+// O(n log n) means sorting by comparisons
+// Non comparison sort
+// Radix sort or counting sort
+
+// Non comparison sorting uses the way that data is sorted.
+// Counting sort and radix sort works with numbers in a specific range.
+
+// Counting sort and radix
+
+// Sometimes if numbers are within a certain it radix sort and counting sort are options

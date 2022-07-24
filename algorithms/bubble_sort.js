edd773c559fa902dc753c224c2ec5ec5022f3f3e
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-24 01:17:15 +0100

    bubble sort

---
+++ algorithms/bubble_sort.js
@@ -0,0 +1,19 @@
+// Bubble sort
+// Bubble Sort
+// Insertion Sort 
+// Selection Sort
+
+// These three algorithms are called elementary sort
+
+// Merge Sort and Quick Sort
+// These are more complex and can be more efficient
+
+// Bubble sort comes from the idea of bubbling up the largest value by using multiple passthroughs
+
+// Bubble sort is not the most efficient sorting algorithm
+// Bubbble sort is one of the simplest sorting algorithms but also one of the most inefficient
+
+// Best time complexity = O(n)
+// Average time complexity = O(n^2)
+// Worst time complexity = O(n^2)
+// Space complexity = O(1) since no new data structures are being created

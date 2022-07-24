e7ef9ac4ac6b67e0959f58a9e2601d414a8208d5
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-24 21:39:25 +0100

    traversal

---
+++ algorithms/searching/traversal.js
@@ -0,0 +1,15 @@
+// Traversals
+// Traversals means going from node to node and making sure that the node being searched for is found
+
+// Traversal and search are often used interchangeably.
+// Sometinmes though, we want to perform a particular operation on each node.
+
+// Or the tree isn't sorted or it's an unordered graph
+// Traversal is visiting every node... that's an O(n) operation.
+
+// Breadth fisrt search/traversal
+// Depth first search/traversal
+
+// The main benefit of why we dont put complex data into jsut lists that are sorted is that we get the O(log n) searching.
+
+// Trees and graphs work well for lots of cases. And there's better performance for something like an array.

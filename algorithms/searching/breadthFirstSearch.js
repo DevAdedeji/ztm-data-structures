efeb93cac6dd9c14f27e1404f3840479d4906aaf
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-24 21:43:49 +0100

    Breadth First Search

---
+++ algorithms/searching/breadthFirstSearch.js
@@ -0,0 +1,6 @@
+// Breadth first search
+
+// Breadth first search starts with the root node and moves left to right across the second level.
+
+// Left to right across the third level etc etc until the element being searched is found.
+// Breadth first search uses additional memory. Because we need to search every node and keep it in track.

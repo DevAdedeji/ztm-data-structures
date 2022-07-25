856e76cbafe3f4e34e3bf99c5f6b87f8204cfc1b
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-25 01:01:51 +0100

    bfs vs dfs

--- algorithms/searching/traversal.js
+++ algorithms/searching/traversal.js
@@ -36,3 +36,21 @@
 // Can get slow
 
 // https://stackoverflow.com/questions/9844193/what-is-the-time-and-space-complexity-of-a-breadth-first-and-depth-first-tree-tr
+
+//If you know a solution is not far from the root of the tree:
+// BFS
+
+//If the tree is very deep and solutions are rare, 
+// BFS (DFS will take long time. )
+
+//If the tree is very wide:
+// DFS (BFS will need too much memory)
+
+//If solutions are frequent but located deep in the tree
+// DFS
+
+//determining whether a path exists between two nodes
+// DFS
+
+//Finding the shortest path
+// BFS

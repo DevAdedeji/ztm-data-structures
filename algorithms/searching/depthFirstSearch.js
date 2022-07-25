b7b2bee55b3af66e8b96aa152d2707c88de7a353
Emmanuel Ayodele <emmayodayo@gmail.com>
2022-07-25 12:23:47 +0100

    pre order, in order and post order

--- algorithms/searching/depthFirstSearch.js
+++ algorithms/searching/depthFirstSearch.js
@@ -5,3 +5,10 @@
 // Depth first search has a lower memory required because it's not necessary to store the child pointers on each level.
 
 // We go depth first with depth first search.
+
+// Depth first search can be implemented in three ways
+// PreOrder, InOrder and PostOrdder
+
+// InOrder gets everything inorder in a binary search tree
+// PreOrder starts from the parent node and grab the child nodes left to right.
+// Post order... going all the way down then going back up to the parent node.
